a682d375fffb1bdb1a808fce327c069874365a56
苏棠 <1945564086@qq.com>
2025-11-30 22:17:50 +0800

    Update hosts.nix

--- hosts/hosts.nix
+++ hosts/hosts.nix
@@ -8,6 +8,6 @@
       nixosVersion = "unstable";
       homeManagerVersion = "master";
     };
-    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqbqHz5O4f6nBoki57c6hekVqUiO4hvSb9k771i61YS";
+    publicKey = " ";
   }
 ]
