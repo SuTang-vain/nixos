2db61c1d1746e15083db41ac635505ac396ea212
苏棠 <1945564086@qq.com>
2025-12-01 20:57:01 +0800

    Update os.nix

--- hosts/inspiron/os.nix
+++ hosts/inspiron/os.nix
@@ -3,4 +3,7 @@
   imports = [ ./hardware-configuration.nix ];
   system.stateVersion = "25.05";
   networking.hostName = host;
+
+  # 启用实验特性（永久生效）
+  nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
 }
